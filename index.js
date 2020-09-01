ccab1dcc024fc33a4c38b2641d58bb4756a18f88
Nelly Sugu <jonly03@gmail.com>
2020-09-01 13:39:27 -0400

    omdb

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const express = require("express");
+const axios = require("axios");
 const server = express();
 
 const PORT = process.env.PORT || 4000;
 
-server.get("/heroku", (req, res) => {
-  res.send("Hello Herokuuu");
+server.get("/movies", (req, res) => {
+  axios
+    .get("http://www.omdbapi.com/?i=tt3896198&apikey=168a6a83")
+    .then((results) => {
+      res.send(results.data);
+    });
 });
 
 server.listen(PORT);
